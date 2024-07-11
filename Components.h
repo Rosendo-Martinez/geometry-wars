f869907a0176c203d3e5563dba355fa5a9275057
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-07-11 18:23:41 -0500

    Add remaining components

--- Components.h
+++ Components.h
@@ -1,4 +1,5 @@
 #include "Vec2.h"
+#include <SFML/Graphics.hpp>
 
 class CTransform
 {
@@ -21,7 +22,8 @@ class CCollision
 public:
     float radius = 0;
 
-    CCollision(float r): radius(r) {}
+    CCollision(float r)
+        : radius(r) {}
 };
 
 class CScore
@@ -29,5 +31,43 @@ class CScore
 public:
     int score = 0;
 
-    CScore(int s): score(s) {}
+    CScore(int s)
+        : score(s) {}
+};
+
+class CShape
+{
+public:
+    sf::CircleShape circle;
+
+    CShape(float radius, int points, const sf::Color & fill, const sf::Color & outline, float thickness)
+        : circle(radius, points) 
+    {
+        circle.setFillColor(fill);
+        circle.setOutlineColor(outline);
+        circle.setOutlineThickness(thickness);
+        circle.setOrigin(radius, radius);
+    }
+};
+
+class CLifespan
+{
+public:
+    int remaining = 0; // remaining lifespan
+    int total = 0; // initial amount of lifespan
+
+    CLifespan(int total)
+        : remaining(total), total(total) {}
+};
+
+class CInput
+{
+public:
+    bool up = false;
+    bool left = false;
+    bool right = false;
+    bool down = false;
+    bool shoot = false;
+
+    CInput() {}
 };
