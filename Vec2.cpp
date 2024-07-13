9cf4af2ac613109472b4ad5d5bc6c8537387430d
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-07-13 14:43:50 -0500

    Updates to Vec2

--- Vec2.cpp
+++ Vec2.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 #include "Vec2.h"
 
-Vec2::Vec2(double x, double y)
+Vec2::Vec2(float x, float y)
     : x(x)
     , y(y)
 {
@@ -36,14 +36,53 @@ Vec2 Vec2::operator / (const Vec2& rhs) const
     return Vec2(x / rhs.x, y / rhs.y);
 }
 
+Vec2 Vec2::operator * (const float val) const
+{
+    return Vec2(x*val, y*val);
+}
+
+void Vec2::operator += (const Vec2& rhs)
+{
+    x += rhs.x;
+    y += rhs.y;
+}
+
+void Vec2::operator -= (const Vec2& rhs)
+{
+    x -= rhs.x;
+    y -= rhs.y;
+}
+
+void Vec2::operator *= (const float val)
+{
+    x *= val;
+    y *= val;
+}
+
+void Vec2::operator /= (const float val)
+{
+    x /= val;
+    y /= val;
+}
+
 void Vec2::normalize()
 {
-    double l = length();
+    float l = length();
     x = x/l;
     y = y/l;
 }
 
-double Vec2::length()
+float Vec2::length() const
 {
     return std::sqrt(x*x + y*y);
+}
+
+float Vec2::dist(const Vec2& v) const
+{
+    return std::sqrt((x - v.x) * (x - v.x) + (y - v.y) * (y - v.y));
+}
+
+float Vec2::distSqr(const Vec2& v) const
+{
+    return (x - v.x) * (x - v.x) + (y - v.y) * (y - v.y);
 }
--- Vec2.h
+++ Vec2.h
@@ -3,18 +3,27 @@
 class Vec2
 {
 public:
-    double x = 0;
-    double y = 0;
+    float x = 0;
+    float y = 0;
 
     bool operator == (const Vec2& rhs) const;
     bool operator != (const Vec2& rhs) const;
+
     Vec2 operator +  (const Vec2& rhs) const;
     Vec2 operator *  (const Vec2& rhs) const;
     Vec2 operator /  (const Vec2& rhs) const;
+    Vec2 operator * (const float val) const;
+
+    void operator += (const Vec2& rhs);
+    void operator -= (const Vec2& rhs);
+    void operator *= (const float val);
+    void operator /= (const float val);
 
     Vec2();
-    Vec2(double x, double y);
+    Vec2(float x, float y);
 
-    void   normalize();
-    double length   ();
+    void   normalize ();
+    float  length    () const;
+    float  dist      (const Vec2& v) const;
+    float  distSqr   (const Vec2& v) const;
 };
