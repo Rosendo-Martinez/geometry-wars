ec76c77e64c02b7458d86af10eed1b00a153cbf4
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-07-17 17:01:25 -0500

    Update() Can Remove Dead Bullets

--- EntityManager.cpp
+++ EntityManager.cpp
@@ -1,4 +1,5 @@
 #include "EntityManager.h"
+#include <algorithm>
 
 std::shared_ptr<Entity> EntityManager::addEntity(const std::string& tag)
 {
@@ -9,17 +10,20 @@ std::shared_ptr<Entity> EntityManager::addEntity(const std::string& tag)
 
 void EntityManager::update()
 {
+    // add entities on wait list
     for (auto e : m_toAdd)
     {
         m_entities.push_back(e);
         m_entityMap[e->tag()].push_back(e);
     }
-    for (auto e : m_entities)
-    {
-        // if e is dead, remove it from m_entities & m_entityMap
-        // Tip: use std removeIf()
-    }
     m_toAdd.clear();
+
+    // remove destroyed bullets
+    const auto newEndEntities = std::remove_if(m_entities.begin(), m_entities.end(), [](const std::shared_ptr<Entity> e){ return !e->isActive(); });
+    m_entities.erase(newEndEntities, m_entities.end());
+    const auto newEndBullets = std::remove_if(m_entityMap["bullet"].begin(), m_entityMap["bullet"].end(), [](const std::shared_ptr<Entity> e){ return !e->isActive(); });
+    m_entityMap["bullet"].erase(newEndBullets, m_entityMap["bullet"].end());
+
 }
 
 EntityVec& EntityManager::getEntities()
