c8a0b24c36c0a13b4332b8704acfa015b46fbdfb
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-10-22 14:49:17 -0500

    Clean Up: Game.h

--- src/Game.h
+++ src/Game.h
@@ -3,7 +3,6 @@
 #include "Entity.h"
 
 
-// Default values are temporary, will read in actual values from file later
 struct PlayerConfig { int SR = 32, CR = 32, FR = 5, FG = 5, FB = 5, OR = 255, OG = 0, OB = 0, OT = 4, V = 8; float S = 5; };
 struct EnemyConfig { int SR = 32, CR = 32, OR = 255, OG = 255, OB = 255, OT = 2, VMIN = 3, VMAX = 8, L = 90, SI = 60, SNE = 50, SSE = 125; float SMIN = 3, SMAX = 3; };
 struct BulletConfig { int SR = 10, CR = 10, FR = 255, FG = 255, FB = 255, OR = 255, OG = 255, OB = 255, OT = 2, V = 20, L = 90; float S = 20; };
@@ -13,7 +12,6 @@ class Game
 {
 public:
     Game(const std::string & config);
-
     void run();
 
 private:
@@ -25,12 +23,12 @@ private:
     EnemyConfig         m_enemyConfig;
     BulletConfig        m_bulletConfig;
     NukeConfig          m_nukeConfig;
-    int                 m_score = 0;
-    int                 m_currentFrame = 0;
-    int                 m_lastEnemySpawnTime = 0;
-    int                 m_lastNukeTime = 0;
-    bool                m_paused = false;
-    bool                m_running = true;
+    int                 m_score                 = 0;
+    int                 m_currentFrame          = 0;
+    int                 m_lastEnemySpawnTime    = 0;
+    int                 m_lastNukeTime          = 0;
+    bool                m_paused                = false;
+    bool                m_running               = true;
 
     std::shared_ptr<Entity> m_player;
 
